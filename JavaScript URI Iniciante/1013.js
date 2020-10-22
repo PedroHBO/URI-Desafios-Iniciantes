2f775db79056f54996e820eb24c8c243dcfd11dd
Pedro Henrique <pedrohbo.trab@gmail.com>
2020-10-22 12:45:14 -0300

    Adicionando 1013 e 1017

---
+++ JavaScript URI Iniciante/1013.js
@@ -0,0 +1,17 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split(' ');
+
+var a = parseInt(lines.shift());
+var b = parseInt(lines.shift());
+var c = parseInt(lines.shift());
+
+var maiorab = (a+b+Math.abs(a-b))/2;
+var maiorbc = (b+c+Math.abs(b-c))/2;
+
+if (maiorbc>= maiorab){
+    console.log(maiorbc + " eh o maior");
+}
+else{
+    console.log(maiorab + " eh o maior");
+}
+
---
+++ JavaScript URI Iniciante/1017.js
@@ -0,0 +1,9 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var km = 12;
+var tempg = parseInt(lines.shift());
+var velm = parseInt(lines.shift());
+
+var litros = (tempg*velm)/km;
+console.log(litros.toFixed(3));
